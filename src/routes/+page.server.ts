3521f4e35ac147076f7626258da2b64108ab9093
cfajfer <fajfer.corey@gmail.com>
2024-12-14 11:11:57 -0500

    feature: add homepage template

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,21 @@
+import * as auth from '$lib/server/auth';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async (event) => {
+	if (!event.locals.user) {
+		return redirect(302, '/login');
+	}
+	return { user: event.locals.user };
+};
+
+export const actions: Actions = {
+	logout: async (event) => {
+		if (!event.locals.session) {
+			return fail(401);
+		}
+		auth.deleteSessionTokenCookie(event);
+
+		return redirect(302, '/login');
+	}
+};
