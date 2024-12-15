58bdb70ecabda7cf79b592537df71ee1e62c3a61
cfajfer <fajfer.corey@gmail.com>
2024-12-15 18:36:43 -0500

    feature: add addWatchlist and deleteWatchlist server actions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,21 +1,39 @@
 import * as auth from '$lib/server/auth';
-import { fail, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
+import * as watchlist from '$lib/server/watchlist';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
 		return redirect(302, '/login');
 	}
-	return { user: event.locals.user };
+
+	const watchlists = await watchlist.getAllWatchlists();
+	const user: string = event.locals.user ?? '';
+
+	if (watchlists.error) {
+		return { user, watchlists: [] };
+	}
+
+	return { user, watchlists: watchlists.data };
 };
 
 export const actions: Actions = {
 	logout: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
 		auth.deleteSessionTokenCookie(event);
 
 		return redirect(302, '/login');
+	},
+	addWatchlist: async (event) => {
+		const formData = await event.request.formData();
+		const watchlistName = formData.get('name') as string;
+
+		await watchlist.createWatchlist(watchlistName);
+	},
+	deleteWatchlist: async (event) => {
+		const formData = await event.request.formData();
+		const watchlistName = formData.get('name') as string;
+
+		await watchlist.deleteWatchlist(watchlistName);
 	}
 };
