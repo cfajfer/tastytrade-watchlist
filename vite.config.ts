efaf6d40629ffe212e5a6fb4409a131233760c3a
cfajfer <fajfer.corey@gmail.com>
2024-12-16 10:11:44 -0500

    install: "lightweight-charts": "^4.2.2" "svelte-lightweight-charts": "^2.2.0"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	// @ts-expect-error - this is a valid Vite config
 	plugins: [sveltekit()],
-
+	ssr: {
+		noExternal: ['svelte-lightweight-charts'] // Exclude the library
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		environment: 'jsdom',
