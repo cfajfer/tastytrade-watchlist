77020c5f9b52a5a334507741807bb16c4a389bb3
cfajfer <fajfer.corey@gmail.com>
2024-12-16 12:05:04 -0500

    style: login button disable/enable styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,11 @@
 	import type { ActionData } from './$types';
 
 	let { form }: { form: ActionData } = $props();
+
+	let username = $state<string>('');
+	let password = $state<string>('');
+
+	let isFormValid = $derived<boolean>(username.length > 0 && password.length > 0);
 </script>
 
 <main
@@ -15,18 +20,31 @@
 			<div class="max-w-lg">
 				<div class="my-3">
 					<div>Username or Email</div>
-					<input name="username" type="text" class="w-full border border-gray-400 p-2" />
+					<input
+						name="username"
+						type="text"
+						bind:value={username}
+						class="w-full border border-gray-400 p-2"
+					/>
 				</div>
 				<div class="my-3">
 					<div>Password</div>
-					<input name="password" type="password" class="w-full border border-gray-400 p-2" />
+					<input
+						name="password"
+						type="password"
+						bind:value={password}
+						class="w-full border border-gray-400 p-2"
+					/>
 				</div>
 				<button
 					type="submit"
 					formaction="?/login"
-					class="my-3 w-full cursor-pointer rounded bg-black p-5 text-white opacity-50"
-					>SIGN IN</button
+					disabled={!isFormValid}
+					class="my-3 w-full cursor-pointer rounded bg-black p-5 text-white opacity-100 shadow transition-all duration-200 disabled:cursor-not-allowed disabled:opacity-50
+					{isFormValid ? 'bg-green-400  hover:bg-green-500' : ''}"
 				>
+					SIGN IN
+				</button>
 			</div>
 		</form>
 		{#if form?.message}
