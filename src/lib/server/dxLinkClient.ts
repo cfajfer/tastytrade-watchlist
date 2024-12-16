d17f08d03672bcc336d629853e6c3ba3e0847097
cfajfer <fajfer.corey@gmail.com>
2024-12-16 09:05:26 -0500

    feature: add api to get dxfeed streamer token

---
+++ src/lib/server/dxLinkClient.ts
@@ -0,0 +1,15 @@
+//! DXLink package is not working https://github.com/dxFeed/dxLink/issues/12
+// Gets the DXFeed streamer token
+export const getDXFeedToken = async (token: string): Promise<string> => {
+	const res = await fetch('https://api.cert.tastyworks.com/api-quote-tokens', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: token
+		}
+	});
+
+	const { data } = await res.json();
+
+	return data;
+};
